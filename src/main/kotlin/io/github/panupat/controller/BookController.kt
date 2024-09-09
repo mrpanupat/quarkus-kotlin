39b51049e16b8ff6a847d146acb1ea4ce93f5490
Panupat Pattararuangtawee <panupat.pat@ascendcorp.com>
2024-09-09 17:03:27 +0700

    Format logger

--- src/main/kotlin/io/github/panupat/controller/BookController.kt
+++ src/main/kotlin/io/github/panupat/controller/BookController.kt
@@ -7,14 +7,20 @@ import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import org.jboss.logging.Logger
 
 @Path("/book")
 class BookController {
+
+    private val logger = Logger.getLogger(BookController::class.java)
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     fun hello(@Valid request: Book.Request): Book.Response {
+
+        logger.info("Book id ${request.id}")
+
         return Book.Response(
             id = request.id,
             name = "This is a book",
